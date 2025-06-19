90fd008d4138998e726cc8efc73a5f998fa1afaf
David_Shonack <dshonack@ucsc.edu>
2025-06-18 21:29:55 -0700

    Deployment improvements implemented log paths

---
+++ MachineLearning_and_AI/Network_Anomoly_Detection/IDS_processor.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+
+def getData(n_seconds): 
+    ''' Function to get data from zeek every n seconds'''
+    # File path = /opt/homebrew/Cellar/zeek/7.1.0/logs/current
+    pass
+def main(): 
+    n_seconds = 60
+    getData(n_seconds)
+    pass
+
+main()
